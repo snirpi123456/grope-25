d41b7c15b85005c2440a3dac68bc374faec742c0
vikarhipskiy <160410423+vikarhipskiy@users.noreply.github.com>
2024-03-05 23:07:14 +0200

    Add files via upload

---
+++ forum.js
@@ -0,0 +1,12 @@
+document.getElementById("btn2").addEventListener("click", function() {
+    document.querySelector("input[type='text']").removeAttribute("disabled");
+    let textValue = document.querySelector("input[type='text']").value
+    document.getElementById("btn1").style.display = "inline-block";
+});
+document.getElementById("btn1").onclick=function(){
+    document.getElementById("btn1").innerHTML="נשלח";
+    document.getElementById("par1").innerHTML="שאלתך נשמרה במערכת";
+    document.getElementById("parQF").value+=textValue;
+
+}
+
