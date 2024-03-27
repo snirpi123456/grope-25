f5d775444c2b6af53c58cd99e2e2d62d2b94da1a
adi stamker <adist1@ac.sce.ac.il>
2024-03-27 19:16:53 +0200

    קישוריות בין עמודי אזור אישי ובניית קובץ מסודר לפונקציות של עמוד בית

---
+++ homepage.js
@@ -0,0 +1,48 @@
+function checkIfUserLoggedIn() {
+    return localStorage.getItem('userData') !== null;
+}
+
+function handleLoginLogout() {
+    if (checkIfUserLoggedIn()) {
+        localStorage.removeItem('userData');
+        alert('התנתקת בהצלחה');
+        window.location.href = 'homepage.html';
+    } else {
+        window.location.href = 'login.html';
+    }
+}
+
+function updateLoginLogoutButton() {
+    const button = document.getElementById('loginLogoutButton');
+    if (checkIfUserLoggedIn()) {
+        button.innerText = 'התנתקות';
+    } else {
+        button.innerText = 'התחברות/הרשמה';
+    }
+    button.style.display = 'block';
+}
+
+function redirectToPersonalAreaOrLogin() {
+    if (checkIfUserLoggedIn()) {
+        window.location.href = 'lawyer_personal_area.html';
+    } else {
+        window.location.href = 'login.html';
+    }
+}
+
+function openForum() {
+    window.location.href = "forum.html";
+}
+
+function openReviews() {
+    window.location.href = "review.html";
+}
+
+function openEmployment() {
+    window.location.href = "תעסוקה.html";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateLoginLogoutButton();
+    document.getElementById('personalAreaButton').addEventListener('click', redirectToPersonalAreaOrLogin);
+});
